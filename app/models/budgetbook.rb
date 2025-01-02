ba03998d7c8e4a69ab798959a8cf036afeac396d
小川道佑 <michisuke@ogawamichiyuunoMacBook-Air.local>
2025-01-02 17:23:29 +0900

    update:家計簿新規作成、編集時のバリデーション設定
    家計簿の新規作成、編集の際、年、月、世帯人数、住居形態は入力必須（無回答という選択肢も含む)とし、その他の項目は0以上の整数を入力するか空欄(答えたくない時などに利用)のみが許可されるようにしました。

--- app/models/budgetbook.rb
+++ app/models/budgetbook.rb
@@ -1,3 +1,23 @@
 class Budgetbook < ApplicationRecord
   belongs_to :user
+  validates :year, presence: true
+  validates :month, presence: true
+  validates :household_size, presence: true
+  validates :housing_type, presence: true
+  validates :annual_income, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :monthly_income, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :food_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :housing_expense, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :utility_bills, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :furniture_cost, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :clothing_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :medical_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :transportation_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :communication_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :education_cost, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :educational_and_entertainment_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :entertainment_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :savings, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :investments, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :others, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
 end
