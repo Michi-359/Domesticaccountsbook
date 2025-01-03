08e40ab25538acc6aa7c7590626e6f4bd65e2221
小川道佑 <michisuke@ogawamichiyuunoMacBook-Air.local>
2025-01-04 02:07:19 +0900

    update:家計簿の一覧・新規作成・編集ページのデザイン修正とbudgetbooks.rbのrubocopエラーの修正
    家計簿の一覧・新規作成・編集ページのボタンや背景の色を修正し、新規作成、編集ページはbootstrapのtableを用いたデザインにしました。
    
    budgetbook.rbのバリデーションの記述について、rubocopで１行が長すぎるというエラーが出ていたため、改行を行いました。

--- app/models/budgetbook.rb
+++ app/models/budgetbook.rb
@@ -4,22 +4,54 @@ class Budgetbook < ApplicationRecord
   validates :month, presence: true
   validates :household_size, presence: true
   validates :housing_type, presence: true
-  validates :annual_income, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :monthly_income, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :food_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :housing_expense, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :utility_bills, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :furniture_cost, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :clothing_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :medical_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :transportation_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :communication_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :education_cost, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :educational_and_entertainment_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :entertainment_expenses, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :savings, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :investments, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
-  validates :others, allow_blank: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000 }
+  validates :annual_income, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :monthly_income, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :food_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :housing_expense, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :utility_bills, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :furniture_cost, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :clothing_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :medical_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :transportation_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :communication_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :education_cost, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :educational_and_entertainment_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :entertainment_expenses, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :savings, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :investments, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
+  validates :others, allow_blank: true, numericality: {
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
+  }
 
   def income_band
     case annual_income
