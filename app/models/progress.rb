dc23dd6b9e7eeb62ea1a8595fc2dd668598f4094
小川道佑 <michisuke@ogawamichiyuunoMacBook-Air.local>
2025-01-09 14:39:51 +0900

    update:貯金目標の達成状況をプログレスバーで表示できるようにする、バリデーションの修正
    貯金目標の達成状況をプログレスバーで表示できるようにするために、毎月貯めた金額の合計を求めるメソッド（total_savingsメソッド）、total_savingsメソッドと毎月貯める金額の他に追加した金額の合計が目標貯金額の何%になっているかを求めるメソッド（progress_percentageメソッド）を作成しました。
    
    貯金目標を達成した場合に、貯金目標の達成状況の表示の仕方を変えるためにprogress_percentageメソッドが100を超えたかどうか判断するメソッド(achieving_purpose?メソッド)を作成しました。
    
    貯金目標一覧ページで貯金の目的と毎月貯める金額の他に追加した金額も常に表示させたいため、これらを入力必須にしました。

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,6 +1,6 @@
 class Progress < ApplicationRecord
   belongs_to :user
-  validates :purpose_of_saving, allow_blank: true, length: { maximum: 100, message: "貯金の目的は100文字以内で入力してください" }
+  validates :purpose_of_saving, presence: { message: "貯金の目的を入力してください" }, length: { minimum: 1, maximum: 100, message: "貯金の目的は1文字以上100文字以内で入力してください" }
   validates :monthly_savings, presence: { message: "毎月貯める金額を入力してください" }, numericality: {
     greater_than_or_equal_to: 1, less_than_or_equal_to: 5000000,
     message: "毎月貯める金額は1円以上500万円以下で入力してください",
@@ -9,12 +9,24 @@ class Progress < ApplicationRecord
     greater_than_or_equal_to: 1, less_than_or_equal_to: 240,
     message: "貯めた月数は1か月以上240か月以下で入力してください",
   }
-  validates :special_reserve, allow_blank: true, numericality: {
+  validates :special_reserve, presence: { message: "毎月貯める金額の他に追加した金額を入力してください" }, numericality: {
     greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000,
-    message: "毎月貯める金額の他に追加した金額は1億円以下で入力してください",
+    message: "毎月貯める金額の他に追加した金額は0円以上1億円以下で入力してください",
   }
   validates :target_amount, presence: { message: "目標貯金額を入力してください" }, numericality: {
     greater_than_or_equal_to: 1, less_than_or_equal_to: 100000000,
     message: "目標貯金額は1円以上1億円以下で入力してください",
   }
+
+  def total_savings
+    monthly_savings * months
+  end
+
+  def progress_percentage
+    (total_savings.to_i + special_reserve.to_i) * 100 / target_amount
+  end
+
+  def achieving_purpose?
+    progress_percentage >= 100
+  end
 end
